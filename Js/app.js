d8e1f11ff18b8a1050b2c00e12e03d7a378b85a2
Pierre-Cinart <cinartpierre@gmail.com>
2023-04-25 12:01:50 +0200

    integration du footer et header en js pour eviter de copier du html sur toute les pages html

--- Js/app.js
+++ Js/app.js
@@ -1,4 +1,49 @@
 const header = document.querySelector('#header')
+const footer = document.querySelector('#footer')
+// integration du header
+header.innerHTML ='<nav class="navbar navbar-expand-lg bg-body-tertiary">'+
+            '<div class="container-fluid">'+
+             '<a class="navbar-brand" href="#">Les jardins de demains</a>'+
+              '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+                '<span class="navbar-toggler-icon"></span>'+
+              '</button>'+
+              '<div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+                '<ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">'+
+                 
+                  '<li class="nav-item">'+
+                    '<a class="nav-link" href="index.html">Acceuil</a>'+
+                  '</li>'+
+                 '<li class="nav-item">'+
+                    '<a class="nav-link" href="./index.html#a-propos">A propos</a>'+
+                  '</li>'+
+                  '<li class="nav-item">'+
+                    '<a class="nav-link" href="contact.html">Contact</a>'+
+                  '</li>'+
+                  '<li class="nav-item">'+
+                    '<a class="nav-link" href="#">Evenements nationaux</a>'+
+                  '</li>'+
+                  
+                '</ul>'+
+              
+              '</div>'+
+            '</div>'+
+          '</nav> '
+// integration du footer
+footer.innerHTML = ' <div class="row ">'+
+'<div class="col-3">'+
+    '<p>lorem5</p>'+
+'</div>'+
+'<div class="col-3">'+
+    '<p>lorem5</p>'+
+'</div>'+
+'<div class="col-3">'+
+    '<p>lorem5</p>'+
+'</div>'+
+'<div class="col-3">'+
+    '<p>lorem5</p>'+
+'</div>'+
+'</div>'+
+'<p class="text-center rights">cinartDev tout droits réservés</p>'
 
 let annuelles = ['Capucine (Tropaeolum majus)',
         'Souci (Calendula officinalis)',
@@ -25,25 +70,43 @@ let annuelles = ['Capucine (Tropaeolum majus)',
         'Tithonia (Tithonia rotundifolia)',
         'Tournesol (Helianthus annuus)',
    ]
+let bisannuelles = [
+    'Pensée (Viola x wittrockiana)',
+    'Muflier (Antirrhinum majus)',
+    'Delphinium (Delphinium spp.)',
+    'Digitale (Digitalis purpurea)',
+    'Giroflée (Matthiola incana)',
+    'Primevère (Primula spp.)',
+    'Rose trémière (Alcea rosea)',
+    'Campanule (Campanula spp.)',
+    'Ancolie (Aquilegia spp.)',
+    'Souci des jardins (Calendula officinalis)',
+    'Bégonia tubéreux (Begonia tuberosa)',
+    'Myosotis (Myosotis spp.)',
+    'Œillet mignardise (Dianthus barbatus)',
+    'Renoncule des jardins (Ranunculus asiaticus)'
+    ]
+bisannuelles = bisannuelles.sort()
+annuelles = annuelles.sort()
 
-   annuelles = annuelles.sort()
- function init(page) {
+
+ function createListFlowers(page){
+    let list = document.getElementById(page)
+    let tab = ''
     switch (page) {
-        case 'annuelles' :{
-            createListAnnuelles();
-            console.log (annuelles)
+        case 'annuelles': {
+            tab = annuelles;
+        }
+        break;
+        case 'bisannuelles': {
+            tab = bisannuelles;
         }
         break;
     }
- }
-
- function createListAnnuelles(){
-    let list = document.getElementById('annuelles')
-    console.log(list)
-    for (let i = 0 ; i < annuelles.length ; i++){
+    for (let i = 0 ; i < tab.length ; i++){
         let li = document.createElement('li')
         li.className = 'fleurs-li'
-        li.textContent = annuelles[i]
+        li.textContent = tab[i]
         list.appendChild(li)
     }
  }
