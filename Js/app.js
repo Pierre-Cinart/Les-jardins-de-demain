073b67c357a8ff6d1d0a66e0c756b457d8be7451
Pierre-Cinart <cinartpierre@gmail.com>
2023-05-01 16:48:49 +0200

    ajout des logo de reseaux sociaux redimension des images pour gagner en temps de chargements des pages

--- Js/app.js
+++ Js/app.js
@@ -3,7 +3,7 @@ const footer = document.querySelector('#footer')
 // integration du header
 header.innerHTML ='<nav class="navbar navbar-expand-lg bg-body-tertiary">'+
             '<div class="container-fluid">'+
-             '<a class="navbar-brand" href="#">Les jardins de demains</a>'+
+             '<a class="navbar-brand" href="index.html">Les jardins de demains</a>'+
               '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
                 '<span class="navbar-toggler-icon"></span>'+
               '</button>'+
@@ -19,7 +19,7 @@ header.innerHTML ='<nav class="navbar navbar-expand-lg bg-body-tertiary">'+
                   '<div class="dropdown-menu" aria-labelledby="navbarDropdown">'+
                   ' <a class="dropdown-item" href="annuelles.html">Nos plantes annuelles</a>'+
                   '<a class="dropdown-item" href="bisannuelles.html">Nos plantes bisannuelles</a>'+
-                  '<a class="dropdown-item" href="vivaces.html">Nos plantes vivvaces</a>'+
+                  '<a class="dropdown-item" href="vivaces.html">Nos plantes vivaces</a>'+
                  '</li>'+
                  '<li class="nav-item">'+
                     '<a class="nav-link" href="./index.html#a-propos">A propos</a>'+
@@ -38,20 +38,33 @@ header.innerHTML ='<nav class="navbar navbar-expand-lg bg-body-tertiary">'+
           '</nav> '
 // integration du footer
 footer.innerHTML = ' <div class="row ">'+
-'<div class="col-3">'+
-    '<p>Exercice de développement de site web pour une association fictive,'+
-    " réalisé dans le cadre de ma formation de développeur web et d'application web chez Studi "+
-    "pour l'année 2023. </p>"+
-'</div>'+
-'<div class="col-3">'+
-    '<p>lorem5</p>'+
-'</div>'+
-'<div class="col-3">'+
-    '<p>lorem5</p>'+
-'</div>'+
-'<div class="col-3">'+
-    '<p>lorem5</p>'+
-'</div>'+
+    '<div class="col-3">'+
+        '<p>Exercice de développement de site web pour une association fictive,'+
+        " réalisé dans le cadre de ma formation de développeur web et d'application web chez Studi "+
+        "pour l'année 2023. </p>"+
+    '</div>'+
+    '<div class="col-3">'+
+        "<p>Association les jardins de demain"+
+        '<br>'+
+        'Adresse : 123 rue des Jardins, 75000 Paris, France'+
+        '<br>'+
+        'Téléphone : +33 1 23 45 67 89'+
+        '<br>'+
+        'Email : contact@lesjardinsdedemain.com '+
+    '</div>'+
+    '<div class="col-6 text-center">'+
+        ' <div class="box-logos "> '+
+            '<a href="https://fr-fr.facebook.com/" target="_blank">'+
+                '<img src="./Images/logo_facebook.png" alt="logo_facebook">'+
+            '</a>'+
+            '<a href="https://twitter.com/accueil?lang=fr" target="_blank">'+
+              ' <img src="./Images/logo_insta.png" alt="logo_insta">'+
+          ' </a>'+
+            '<a href="https://www.instagram.com/" target="_blank">'+
+              ' <img src="./Images/logo_twitter.png" alt="logo_twitter">'+
+          ' </a>'+
+        '</div>'+
+    '</div>'+
 '</div>'+
 '<p class="text-center rights">cinartDev tout droits réservés</p>'
 
@@ -96,8 +109,27 @@ let bisannuelles = [
     'Œillet mignardise (Dianthus barbatus)',
     'Renoncule des jardins (Ranunculus asiaticus)'
     ]
+let vivaces = [
+  'Lavande (Lavandula angustifolia)',
+  'Sauge (Salvia officinalis)',
+  'Thym (Thymus vulgaris)',
+  'Romarin (Rosmarinus officinalis)',
+  'Menthe (Mentha spicata)',
+  'Persil (Petroselinum crispum)',
+  'Ciboulette (Allium schoenoprasum)',
+  'Estragon (Artemisia dracunculus)',
+  'Origan (Origanum vulgare)',
+  'Camomille (Matricaria chamomilla)',
+  'Mauve (Malva sylvestris)',
+  'Achillée millefeuille (Achillea millefolium)',
+  'Pensée sauvage (Viola tricolor)',
+  'Primevère (Primula vulgaris)',
+  'Oeillet (Dianthus caryophyllus)',
+  'Astilbe (Astilbe spp.)'
+]
 bisannuelles = bisannuelles.sort()
 annuelles = annuelles.sort()
+vivaces = vivaces.sort()
 
 
  function createListFlowers(page){
@@ -112,6 +144,10 @@ annuelles = annuelles.sort()
             tab = bisannuelles;
         }
         break;
+        case 'vivaces': {
+          tab = vivaces;
+      }
+      break;
     }
     for (let i = 0 ; i < tab.length ; i++){
         let li = document.createElement('li')
